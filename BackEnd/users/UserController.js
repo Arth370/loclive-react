847a2664671acd4aef355ca77198066bf43c6ae1
Mateus Fernandes <josemateus0925@gmail.com>
2025-11-21 09:45:31 -0300

    login backend

--- BackEnd/users/UserController.js
+++ BackEnd/users/UserController.js
@@ -6,7 +6,7 @@ export const listarUsers = async(req,res,next)=>{
             attributes:["id","nome","email","telefone","senha"],
             order:[["created_at","DESC"]]
         })
-        res.status(200).json([users])
+        res.status(200).json({users})
     }catch(error){
         next(error)
     }
@@ -41,6 +41,36 @@ export const criarUsers = async (req,res,next)=>{
 
         const novoUser = await userModel.create({nome,email,telefone,senha})
         res.status(201).json({mensagem: "Usuário Criado",novoUser})
+    }catch(error){
+        next(error)
+    }
+}
+export const attUser = async (req,res,next)=>{
+    const id = req.params.id
+    const {nome,email,telefone,senha} = req.body
+    try{
+        if(!id){
+            const err = new Error("o ID é obrigatório")
+            err.statusCode = 400
+            throw err
+        }
+        const user = await userModel.findByPk(id,{
+            attributes:["id", "nome", "email", "telefone", "senha"]
+        })
+        if(nome !== undefined){
+            user.nome = nome
+        }
+        if(email !== undefined){
+            user.email = email
+        }
+        if(telefone !== undefined){
+            user.telefone = telefone
+        }
+        if(senha !== undefined){
+            user.senha = senha
+        }
+        await user.save();
+        res.status(200).json({user})
     }catch(error){
         next(error)
     }
--- router/Login.jsx
+++ router/Login.jsx
@@ -1,7 +1,48 @@
+import {useEffect, useState} from 'react'
 import React from 'react'
 import '../router/Login.css'
 import { Link } from 'react-router-dom'
+import { use } from 'react'
 function Login() {
+    const [users, setUsers] = useState([])
+    const [loading,setLoading] = useState(true)
+    const [email, setEmail] = useState("")
+    const [senha,setSenha] = useState("")
+
+    useEffect(()=>{
+        const fetchUsers = async ()=>{
+            try{
+                const coisa = await fetch('http://localhost:3333/api/users')
+                if(!coisa.ok){
+                    throw new Error("error a buscar tarefas")
+                }
+                const data = await coisa.json()
+                console.log(data.users)
+                setUsers(data.users)
+            }catch(error){
+                console.log(error)
+            }finally{
+                setLoading(false)
+              }
+            }
+        fetchUsers()
+    },[setLoading])
+    function handleclick(){
+        console.log(users.find((element)=> element.email == email))
+        if(users.find((element)=> element.email == email)!=undefined){
+            const confirmar = users.find((element)=> element.email == email)
+            if(senha == confirmar.senha){
+                window.location.href='http://localhost:5173/produtos'
+                localStorage.getItem('email',setEmail)
+                localStorage.getItem('senha',setSenha)
+            }
+            else{
+                alert('Senha incorreta')
+            }
+        }else{
+            alert("Email não cadastrado")
+        }
+    }
     return (
         <>
             <div class="FundoLogin">
@@ -10,18 +51,18 @@ function Login() {
                 </div>
                 <div class="EmailL">
                     <h2>Email<span>*</span></h2>
-                    <input class="Input" type="text" />
+                    <input class="Input" type="text" value={email} onChange={event => setEmail(event.target.value )} />
                 </div>
                 <div class="SenhaL">
                     <h2>Senha<span>*</span></h2>
-                    <input class="Input" type="text" />
+                    <input class="Input" type="text" value={senha} onChange={event => setSenha(event.target.value)} />
                 </div>
                 <div class="ConcordoL">
                     <input type="checkbox" />
                     <h4>Eu concordo em receber conteúdo promocional relacionado<br></br> com os produtos e serviços da LocLive</h4>
                 </div>
                 <div class="CentroL">
-                    <button id="botaoL"><h2>Entrar</h2></button>
+                    <button id="botaoL"onClick={handleclick}><h2>Entrar</h2></button>
                     <div id="perguntaL">
                         <h4>Não tem cadastro?</h4>
                         <Link class="linkL" to={'/cadastro'}><h4 class="EntrarL">Cadastrar</h4></Link>
