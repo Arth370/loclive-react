4e861217dded479baeff72874d2f2d4f65ae55d4
Danilo Hipólito Barbosa Jatobá <danilohipolito.jatoba@gmail.com>
2025-12-01 11:26:13 -0300

    mensagem

--- router/Perfil.jsx
+++ router/Perfil.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Cabecario from './Cabecario';
+import Footer from './Footer';
+
 
 function Perfil() {
   return (
     <>
-      
+        <Cabecario/>
+        <div id='separar'></div>
+
+        
+
+        <div id='separar'></div>
+        <Footer/>
     </>
   )
 }
